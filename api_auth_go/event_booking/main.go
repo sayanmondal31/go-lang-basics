9ee866951c0f54cf78eac41a59d9d3d9024fcaa5
drivadosayan <techsupport1@drivado.com>
2025-10-04 11:37:07 +0530

    update

--- api_auth_go/event_booking/main.go
+++ api_auth_go/event_booking/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sayanmondal31/event_booking/db"
 	"sayanmondal31/event_booking/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	server.POST("/event", createEvent)
 
 	server.Run(":8080") // listening in some domain or for dev it is localhost:8080
 
@@ -24,6 +27,19 @@ func getEvents(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 	var event models.Event
-	context.ShouldBindJSON(&event)
+	fmt.Println(context, "context")
+	err := context.ShouldBindJSON(&event) // kind of like scan from fmt
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data", "err": err})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 
 }
--- api_auth_go/event_booking/models/event.go
+++ api_auth_go/event_booking/models/event.go
@@ -1,24 +1,45 @@
 package models
 
 import (
+	"sayanmondal31/event_booking/db"
 	"time"
 )
 
 type Event struct {
-	ID          int
-	Name        string    `json: name, binding:"required"`
-	Description string    `json: description, binding:"required"`
-	Location    string    `json: location, binding:"required"`
-	DateTime    time.Time `json: dateTime, binding:"required"`
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserId      int
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
+func (e Event) Save() error {
 	// later: add it to database
-	events = append(events, e)
+	query := `INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?,?,?,?,?)
+	`
 
+	stmt, err := db.DB.Prepare(query)
+
+	defer stmt.Close()
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
 }
 
 func GetAllEvents() []Event {
